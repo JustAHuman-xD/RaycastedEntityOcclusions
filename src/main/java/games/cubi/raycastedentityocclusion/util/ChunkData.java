bfc2579b3c707fd0fcc22c7f14e2c83b16f4bc18
JustAHuman-xD <imjustahuman.xd@gmail.com>
2025-07-10 14:18:05 -0500

    this does need to be concurrent shucks

--- src/main/java/games/cubi/raycastedentityocclusion/util/ChunkData.java
+++ src/main/java/games/cubi/raycastedentityocclusion/util/ChunkData.java
@@ -2,12 +2,12 @@ package games.cubi.raycastedentityocclusion.util;
 
 import org.bukkit.ChunkSnapshot;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ChunkData {
     public final ChunkSnapshot snapshot;
-    public final Map<BlockPos, Boolean> occluding = new HashMap<>();
+    public final Map<BlockPos, Boolean> occluding = new ConcurrentHashMap<>();
     public long lastRefresh;
 
     public ChunkData(ChunkSnapshot snapshot, long time) {
